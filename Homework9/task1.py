40e39d8c421d99f881d5463515692e3aeff7eec1
Verezub Khristina <k.le-fay@yandex.ru>
2023-06-20 22:34:25 +0300

    Homework9 - task1

---
+++ Homework9/task1.py
@@ -0,0 +1,27 @@
+# Дан текстовый файл test_file/task1_data.txt
+# Он содержит текст, в словах которого есть цифры.
+# Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
+
+# Здесь пишем код
+
+with open('test_file/task1_data.txt', encoding='utf-8') as task1_data:
+    list_lines = task1_data.readlines()
+    exclude = [str(x) for x in range(10)]
+    new_txt = ''
+    for items in list_lines:
+        for letter in items:
+            if letter not in exclude:
+                new_txt += letter
+
+with open('test_file/task1_answer.txt', mode='w', encoding='utf-8') as result:
+    result.write(new_txt)
+
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
